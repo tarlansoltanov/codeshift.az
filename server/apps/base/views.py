2b1741e9948cd185986c483c69d374e4a2c0788f
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-04-21 14:00:32 +0200

    About Template, View and URLs added

--- server/apps/base/views.py
+++ server/apps/base/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request, template_name='base/index.html', context={}):
     context['title'] = 'Home'
     return render(request, template_name, context)
+
+
+def about(request, template_name='base/about.html', context={}):
+    context['title'] = 'About'
+    return render(request, template_name, context)
