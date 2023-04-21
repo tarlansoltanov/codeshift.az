bb30c7b788df6da842dc3666a02758c962a2b3cd
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-04-21 16:19:27 +0200

    Category Model added and registered

--- server/apps/portfolio/models.py
+++ server/apps/portfolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Model definition for Category."""
+
+    name = models.CharField("Name", max_length=50)
+
+    class Meta:
+        """Meta definition for Category."""
+
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        """Unicode representation of Category."""
+        return f'{self.name}'
