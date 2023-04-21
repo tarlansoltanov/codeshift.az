4c319ba4641f7d5f950cfe03967a1bc1ec9b592b
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-04-21 16:04:42 +0200

    Contact Details Model added and registered

--- server/apps/base/admin.py
+++ server/apps/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone', 'address', 'instagram', 'facebook')
+
+    def has_add_permission(self, request):
+        return not self.model.objects.exists()
--- server/apps/base/models.py
+++ server/apps/base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """Model definition for Contact."""
+
+    email = models.EmailField("Email", max_length=254)
+    phone = models.CharField("Phone", max_length=50)
+    address = models.CharField("Address", max_length=50)
+    instagram = models.URLField("Instagram URL", max_length=200)
+    facebook = models.URLField("Facebook URL", max_length=200)
+
+    class Meta:
+        """Meta definition for Contact."""
+
+        verbose_name = 'Contact Details'
+        verbose_name_plural = 'Contact Details'
+
+    def __str__(self):
+        """Unicode representation of Contact."""
+        return f"Contact Details"
