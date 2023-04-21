34191ee05e16abaf9616d6ac5bd16cc134c5feaa
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-04-21 17:04:37 +0200

    Project and ProjectImage models added and registered

--- server/apps/portfolio/admin.py
+++ server/apps/portfolio/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Category
+from .models import Project, ProjectImage, Category
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
+
+class ProjectImageInline(admin.StackedInline):
+    model = ProjectImage
+    extra = 1
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title', 'category', 'client', 'website')
+    inlines = [ProjectImageInline]
--- server/apps/portfolio/models.py
+++ server/apps/portfolio/models.py
@@ -1,6 +1,44 @@
 from django.db import models
 
 
+class Project(models.Model):
+    """Model definition for Project."""
+
+    name = models.CharField("Name", max_length=50)
+    title = models.CharField("Title", max_length=250)
+    description = models.TextField("Description")
+    client = models.CharField("Client", max_length=50)
+    category = models.ForeignKey("Category", verbose_name="Category", on_delete=models.CASCADE, related_name="projects")
+    website = models.CharField("Website", max_length=250)
+
+    class Meta:
+        """Meta definition for Project."""
+
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        """Unicode representation of Project."""
+        return f'{self.name}'
+
+
+class ProjectImage(models.Model):
+    """Model definition for ProjectImage."""
+
+    image = models.ImageField("Image", upload_to='portfolio/images')
+    project = models.ForeignKey("Project", verbose_name="Project", on_delete=models.CASCADE, related_name="images")
+
+    class Meta:
+        """Meta definition for ProjectImage."""
+
+        verbose_name = 'ProjectImage'
+        verbose_name_plural = 'ProjectImages'
+
+    def __str__(self):
+        """Unicode representation of ProjectImage."""
+        return f'{self.project.name}'
+
+
 class Category(models.Model):
     """Model definition for Category."""
 
