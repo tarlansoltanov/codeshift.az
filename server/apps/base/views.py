ef5373bee0c11984e1cb8723e7d5d16f2db86348
Tarlan Soltanov <tarlansoltanov@outlook.com>
2023-04-24 11:13:11 +0200

    Contact Form added

--- server/apps/base/views.py
+++ server/apps/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from ..portfolio.models import Project
+from .models import Message
 
 
 def home(request, template_name='base/index.html', context={}):
@@ -17,4 +18,25 @@ def about(request, template_name='base/about.html', context={}):
 
 def contact(request, template_name='base/contact.html', context={}):
     context['title'] = 'Contact'
+
+    if request.method == 'POST' and request.accepts("application/json"):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        if name and email and phone and subject and message:
+            Message.objects.create(
+                name=name,
+                email=email,
+                phone=phone,
+                subject=subject,
+                message=message
+            )
+
+            return HttpResponse('Message sent successfully')
+
+        return HttpResponse('Please fill all the fields', status=400)
+
     return render(request, template_name, context)
